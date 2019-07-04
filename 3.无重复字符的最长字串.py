735d6c20e5be3c4312cc1eb92f0dfd3ca37d65ad
李宽洋 <lky190249@gmail.com>
2019-07-04 16:54:57 +0800

    3.无重复字符的最长字串

--- 3.无重复字符的最长字串.py
+++ 3.无重复字符的最长字串.py
@@ -6,6 +6,7 @@ class Solution:
         for num, char in enumerate(s):  
             if char in used_char and used_char[char]>=start:  #字符在使用的映射中，且该字符在窗口中
                 start = used_char[char]+1 #重置重复位置
+            else:
+                res = max(res,num-start+1)  #计算结果
             used_char[char] = num  #字符映射更新
-            res = max(res,num-start+1)
         return res
