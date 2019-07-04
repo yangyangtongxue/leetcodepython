30cd2073730bcd38ebef7c042cd3bd8fd1378d15
李宽洋 <lky190249@gmail.com>
2019-07-04 16:41:55 +0800

    3.无重复字符的最长字串

---
+++ 3.无重复字符的最长字串.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        used_char = {} #字符的映射
+        start = 0  #重复位置
+        res = 0  #结果
+        for num, char in enumerate(s):  
+            if char in used_char and used_char[char]>=start:  #字符在使用的映射中，且该字符在窗口中
+                start = used_char[char]+1 #重置重复位置
+            used_char[char] = num  #字符映射更新
+            res = max(res,num-start+1)
+        return res
